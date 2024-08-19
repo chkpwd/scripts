963eecfea778df1e89b86b83f4b6c5cec2ac8e9b
Bryan Jones <unix.chkpwd@gmail.com>
2024-08-19 11:33:29 -0400

    add pre-commits and global pyproject

---
+++ pre-commit/move_files.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+
+"""Move files to another repo database"""
+
+import shutil
+from pathlib import Path
+
+if __name__ == "__main__":
+    src_file = Path.home().joinpath(
+        "code", "scripts", "python", "ente-totp-genearator", "main.py"
+    )
+    dest_file = Path.home().joinpath(
+        "code", "alfred-workflows", "ente-totp", "main.py"
+    )
+
+    shutil.copy(src_file, dest_file)
